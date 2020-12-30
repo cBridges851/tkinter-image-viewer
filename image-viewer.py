dda35632acf606ac0f41264588883fb3a81850f2
Christa Bridges <christa@pebblepad.co.uk>
2020-12-30 11:59:33 +0000

    interface-design Changed variable name and added a space below a function.

--- image-viewer.py
+++ image-viewer.py
@@ -10,8 +10,8 @@ name_label = Label(root, text="Image1.png", bg="#1D1D1D", fg="#FFFFFF", font=("A
 name_label.grid(row=0, column=0, columnspan=3)
 
 # Label for Display
-placeholder = ImageTk.PhotoImage(Image.open("./images/image1.png"))
-display = Label(image=placeholder, width=500, height=300, bg="#1D1D1D")
+current_image = ImageTk.PhotoImage(Image.open("./images/image1.png"))
+display = Label(image=current_image, width=500, height=300, bg="#1D1D1D")
 display.grid(row=1, column=1)
 
 # Buttons
@@ -20,6 +20,7 @@ def move_left():
 
 def move_right():
     print("Move right")
+
 move_left_button = Button(root, text="<", font=("Calibri 20"), bg="#1C1C1C", fg="#FFFFFF")
 move_left_button.grid(row=1, column=0)
 move_right_button = Button(root, text=">", font=("Calibri 20"), bg="#1C1C1C", fg="#FFFFFF")
