1f43e8fa847802d869c9615bcb4f7500dd0ee7f6
heySanj <saprasad92@gmail.com>
2022-10-06 15:43:26 +1000

    Cleaned up excessive comments and streamlined code

--- image-viewer.py
+++ image-viewer.py
@@ -5,13 +5,13 @@ from os.path import isfile
 
 class ImageViewer():
     def __init__(self):
-                     
+        
+        default_directory = "./images"             
         self.current_image_index = 0
         self.image_files = []
-        self.blank_img = Image.new('RGBA',(500,300),(0,0,0,0)) # Blank Image to display if no other images are found   
-        self.img_dir = "./images" # Directory to search images in -- by default is the ./images folder
+        self.blank_img = Image.new('RGBA',(500,300),(0,0,0,0))
+        self.img_dir = default_directory
 
-        # Tkinter elements setup
         self.root = Tk()
         self.name_label = Label(self.root, text="No image found", bg="#1D1D1D", fg="#FFFFFF", font=("Arial 18"))
         self.current_image = ImageTk.PhotoImage(self.blank_img)
@@ -19,20 +19,18 @@ class ImageViewer():
         self.move_left_button = Button(self.root, text="<", font=("Calibri 20"), bg="#1C1C1C", fg="#FFFFFF", command=self.move_left)
         self.move_right_button = Button(self.root, text=">", font=("Calibri 20"), bg="#1C1C1C", fg="#FFFFFF", command=self.move_right)
                 
-        # Menu creation
+
         self.create_menu()
-        
-        # Refresh the current directory and display images
         self.refresh()
         
 
     def move_left(self):
-        if self.current_image_index != 0 and len(self.image_files) != 0:
+        if self.current_image_index != 0:
             self.current_image_index -= 1
             self.display_image()
 
     def move_right(self):
-        if self.current_image_index != len(self.image_files) - 1 and len(self.image_files) != 0:
+        if self.current_image_index != len(self.image_files) - 1:
             self.current_image_index += 1
             self.display_image()
 
@@ -59,63 +57,54 @@ class ImageViewer():
         self.root.bind("<KeyPress>", self.onKeyPress)
         self.root.mainloop()
         
-    # Menu creation
     def create_menu(self):
-        menubar = Menu(self.root) # Create the main Menu bar
-        self.root.config(menu=menubar) # Attach the Menu bar to the main root window
+        menubar = Menu(self.root)
+        self.root.config(menu=menubar)
         
-        file_menu = Menu(menubar, tearoff=False) # Create the 'File' sub menu
-        file_menu.add_command(label='Open Folder', command=self.open_folder, underline=0) # Open Folder
-        file_menu.add_command(label='Refresh', command=self.refresh, underline=0) # Refresh the current folder
-        file_menu.add_command(label='Exit', command=self.root.destroy, underline=1) # Add the Exit button to the File menu
-        menubar.add_cascade(label='File', menu=file_menu, underline=0) # Display the File button on the menu bar
+        file_menu = Menu(menubar, tearoff=False)
+        file_menu.add_command(label='Open Folder', command=self.open_folder, underline=0)
+        file_menu.add_command(label='Refresh', command=self.refresh, underline=0)
+        file_menu.add_command(label='Exit', command=self.root.destroy, underline=1)
+        menubar.add_cascade(label='File', menu=file_menu, underline=0)
     
-    # Open the explorer and choose a new folder    
     def open_folder(self):
         new_dir = filedialog.askdirectory()
         
-        # only change directory if a new one is selected
         if new_dir != "": 
             self.img_dir = new_dir
             self.find_images(self.img_dir)
     
-    # Refresh the current directory, incase images have been added or removed
     def refresh(self):
         self.find_images(self.img_dir)
     
-    # Find images in input directory and append to image_files list    
+
     def find_images(self, dir):
         
-        # Clear current list of images and reset index
         self.image_files = []
         self.current_image_index = 0
         
         for item in listdir(dir):
             if isfile(f"{dir}/{item}"):
-                # Only add image items with the following extensions
                 if(item.lower().endswith(('.png', '.jpg', '.jpeg', '.ico', '.gif'))):
                     self.image_files.append(item)
                 
-        # Display
         self.display_image()
 
-    # Display image at the current index
+
     def display_image(self):
         
         try:
-            self.name_label.configure(text=self.image_files[self.current_image_index]) # Update the Label
-            new_img = Image.open(f"{self.img_dir}/{self.image_files[self.current_image_index]}") # Open the image
-            self.current_image = ImageTk.PhotoImage(self.resize_image(new_img)) # Resize and load the image
-            self.display.configure(image=self.current_image) # Display the image
+            self.name_label.configure(text=self.image_files[self.current_image_index])
+            new_img = Image.open(f"{self.img_dir}/{self.image_files[self.current_image_index]}")
+            self.current_image = ImageTk.PhotoImage(self.resize_image(new_img))
+            self.display.configure(image=self.current_image)
             
         except OSError as e:
-            # No images were found - so change the label and display a blank image
             self.name_label.configure(text=e)
             self.current_image = ImageTk.PhotoImage(self.blank_img)
             self.display.configure(image=self.current_image)
             
         except IndexError:
-            # No images were found - so change the label and display a blank image
             self.name_label.configure(text="No image files found")
             self.current_image = ImageTk.PhotoImage(self.blank_img)
             self.display.configure(image=self.current_image)
@@ -128,12 +117,11 @@ class ImageViewer():
             new_width = aspect_ratio * 300
             return image.resize((int(new_width), 300), Image.Resampling.LANCZOS)
         
-        elif image.width > 500:
+        if image.width > 500:
             aspect_ratio = image.width / image.height
             new_height = 500 * aspect_ratio
             return image.resize((500, int(new_height)), Image.Resampling.LANCZOS)
         
-        else:
-            return image
+        return image
 
 ImageViewer().render()
